defc35f5b544349b8f705ad921ed455f9a067662
adnan <adnan4456@gmail.com>
2024-03-09 01:12:33 +0100

    removed rich text from AddEditNoteScreen.kt file

--- app/src/main/java/com/example/notebook/feature_note/presentation/add_edit_note/ui/AddEditNoteScreen.kt
+++ app/src/main/java/com/example/notebook/feature_note/presentation/add_edit_note/ui/AddEditNoteScreen.kt
@@ -144,60 +144,60 @@ fun AddEditNoteScreen(
             }
             
             Spacer(modifier = Modifier.height(24.dp))
-            EditorControls(
-                modifier = Modifier.weight(1f),
-                state = state,
-                onBoldClick = {
-                    state.toggleSpanStyle(SpanStyle(fontWeight = FontWeight.Bold))
-                },
-                onItalicClick = {
-                    state.toggleSpanStyle(SpanStyle(fontStyle = FontStyle.Italic))
-                },
-                onUnderlineClick = {
-                    state.toggleSpanStyle(SpanStyle(textDecoration = TextDecoration.Underline))
-                },
-                onTitleClick = {
-                    state.toggleSpanStyle(SpanStyle(fontSize = titleSize))
-                },
-                onSubtitleClick = {
-                    state.toggleSpanStyle(SpanStyle(fontSize = subtitleSize))
-                },
-                onTextColorClick = {
-                    state.toggleSpanStyle(SpanStyle(color = Color.Red))
-                },
-                onStartAlignClick = {
-                    state.toggleParagraphStyle(ParagraphStyle(textAlign = TextAlign.Start))
-                },
-                onEndAlignClick = {
-                    state.toggleParagraphStyle(ParagraphStyle(textAlign = TextAlign.End))
-                },
-                onCenterAlignClick = {
-                    state.toggleParagraphStyle(ParagraphStyle(textAlign = TextAlign.Center))
-                },
-                onExportClick = {
-                    Log.d("Editor", state.toHtml())
-                }
-            )
-            RichTextEditor(
-                modifier = Modifier
-                    .background(
-                        color = noteBackgroundAnimatable.value
-                    )
-                    .fillMaxWidth()
-                    .weight(1f),
-                singleLine = true,
-                state = stateTitle,
-            )
-            Spacer(modifier = Modifier.width(8.dp))
-            RichTextEditor(
-                modifier = Modifier
-                    .background(
-                        color = noteBackgroundAnimatable.value
-                    )
-                    .fillMaxWidth()
-                    .weight(8f),
-                state = state,
-            )
+//            EditorControls(
+//                modifier = Modifier.weight(1f),
+//                state = state,
+//                onBoldClick = {
+//                    state.toggleSpanStyle(SpanStyle(fontWeight = FontWeight.Bold))
+//                },
+//                onItalicClick = {
+//                    state.toggleSpanStyle(SpanStyle(fontStyle = FontStyle.Italic))
+//                },
+//                onUnderlineClick = {
+//                    state.toggleSpanStyle(SpanStyle(textDecoration = TextDecoration.Underline))
+//                },
+//                onTitleClick = {
+//                    state.toggleSpanStyle(SpanStyle(fontSize = titleSize))
+//                },
+//                onSubtitleClick = {
+//                    state.toggleSpanStyle(SpanStyle(fontSize = subtitleSize))
+//                },
+//                onTextColorClick = {
+//                    state.toggleSpanStyle(SpanStyle(color = Color.Red))
+//                },
+//                onStartAlignClick = {
+//                    state.toggleParagraphStyle(ParagraphStyle(textAlign = TextAlign.Start))
+//                },
+//                onEndAlignClick = {
+//                    state.toggleParagraphStyle(ParagraphStyle(textAlign = TextAlign.End))
+//                },
+//                onCenterAlignClick = {
+//                    state.toggleParagraphStyle(ParagraphStyle(textAlign = TextAlign.Center))
+//                },
+//                onExportClick = {
+//                    Log.d("Editor", state.toHtml())
+//                }
+//            )
+//            RichTextEditor(
+//                modifier = Modifier
+//                    .background(
+//                        color = noteBackgroundAnimatable.value
+//                    )
+//                    .fillMaxWidth()
+//                    .weight(1f),
+//                singleLine = true,
+//                state = stateTitle,
+//            )
+//            Spacer(modifier = Modifier.width(8.dp))
+//            RichTextEditor(
+//                modifier = Modifier
+//                    .background(
+//                        color = noteBackgroundAnimatable.value
+//                    )
+//                    .fillMaxWidth()
+//                    .weight(8f),
+//                state = state,
+//            )
 
 
             Spacer(modifier = Modifier.height(24.dp))
@@ -241,24 +241,24 @@ fun AddEditNoteScreen(
             }
 
             Spacer(modifier = Modifier.height(16.dp))
-//            TransparentContentTextField(
-//                modifier = Modifier.background(
-//                    color = noteBackgroundAnimatable.value
-//                ),
-//                text =contentState.text ,
-//                hint = contentState.hint,
-//                onValueChanged = {
-//                    viewModel.onEvent(AddEditNoteEvent.EnterContent(it))
-//                },
-//                onFocusChange = {
-//                    viewModel.onEvent(AddEditNoteEvent.ChangeContentFocus(it))
-//                },
-//                isHintVisible = contentState.isHintVisible,
-//                singleLine = false,
-//                textStyle = TextStyle(
-//                    fontSize = 20.sp
-//                ),
-//            )
+            TransparentContentTextField(
+                modifier = Modifier.background(
+                    color = noteBackgroundAnimatable.value
+                ),
+                text =contentState.text ,
+                hint = contentState.hint,
+                onValueChanged = {
+                    viewModel.onEvent(AddEditNoteEvent.EnterContent(it))
+                },
+                onFocusChange = {
+                    viewModel.onEvent(AddEditNoteEvent.ChangeContentFocus(it))
+                },
+                isHintVisible = contentState.isHintVisible,
+                singleLine = false,
+                textStyle = TextStyle(
+                    fontSize = 20.sp
+                ),
+            )
         }
     }
 }
